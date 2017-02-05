0a2a467ab92a542ffda73c1d91043015256b762c
Addison Luh <hueich@gmail.com>
2017-02-04 23:56:40 -0800

    Add prompt for player's move, so an actual game can be played

--- play.go
+++ play.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"os"
+	"bufio"
 	"fmt"
 	"log"
 	"strings"
+	"strconv"
 
 	"github.com/hueich/blokus"
 )
@@ -30,6 +33,10 @@ func getColorTermSymbol(c blokus.Color) string {
 	return fmt.Sprintf("\033[1;%dm%c\033[0m", getColorAsciiCode(c), strings.ToUpper(c.String())[0])
 }
 
+func highlightString(s string) string {
+	return fmt.Sprintf("\033[1m%s\033[0m", s)
+}
+
 func renderBoard(b *blokus.Board) {
 	div := fmt.Sprintf("+%s", strings.Repeat("---+", len(b.Grid())))
 	fmt.Println(div)
@@ -43,11 +50,18 @@ func renderBoard(b *blokus.Board) {
 	}
 }
 
+func fscanln(r *bufio.Reader, a ...interface{}) error {
+	r.Discard(r.Buffered())
+	_, err := fmt.Fscanln(r, a...)
+	return err
+}
+
 func promptForNewPlayers(g *blokus.Game) error {
+	stdin := bufio.NewReader(os.Stdin)
 	numPlayers := 0
 	for numPlayers < 2 || numPlayers > 4 {
 		fmt.Printf("How many players? [2-4]: ")
-		if _, err := fmt.Scanln(&numPlayers); err != nil {
+		if err := fscanln(stdin, &numPlayers); err != nil {
 			fmt.Println("Sorry, I don't know what that number is.")
 			continue
 		}
@@ -65,7 +79,6 @@ func promptForNewPlayers(g *blokus.Game) error {
 		{len(g.Board().Grid())-1,len(g.Board().Grid()[0])-1},
 		{len(g.Board().Grid())-1,0},
 	}
-	fmt.Println(startPositions)
 	if numPlayers == 2 {
 		// Make 2nd player start diagonally across from first player.
 		startPositions = append(startPositions[:1], startPositions[2])
@@ -73,9 +86,9 @@ func promptForNewPlayers(g *blokus.Game) error {
 
 	for i := 1; i <= numPlayers; i++ {
 		var name string
-		for name == "" {
+		for true {
 			fmt.Printf("Enter name of player %d: ", i)
-			if _, err := fmt.Scanln(&name); err != nil {
+			if err := fscanln(stdin, &name); err != nil {
 				fmt.Println("Sorry, I didn't catch the name.")
 				continue
 			}
@@ -84,13 +97,79 @@ func promptForNewPlayers(g *blokus.Game) error {
 				fmt.Println("Sorry, the name can't be empty.")
 				continue
 			}
+			color := blokus.Color(i)
+			startPos := startPositions[i-1]
+			if err := g.AddPlayer(name, color, startPos); err != nil {
+				fmt.Printf("Sorry, I couldn't add the player. %v\n", err)
+				continue
+			}
+			fmt.Printf("Player %s is color %v and will start at coordinate %v\n", highlightString(name), color, startPos)
+			break
 		}
-		color := blokus.Color(i)
-		startPos := startPositions[i-1]
-		if err := g.AddPlayer(name, color, startPos); err != nil {
-			return err
+	}
+	return nil
+}
+
+func promptForNextMove(g *blokus.Game) error {
+	stdin := bufio.NewReader(os.Stdin)
+	player := g.CurrentPlayer()
+
+	// TODO: Render available blocks
+
+	var input string
+	for true {
+		fmt.Printf("It's player %s's turn. Which piece do you want to play? (Type 'pass' to pass your turn): ", highlightString(player.Name()))
+		if err := fscanln(stdin, &input); err != nil {
+			fmt.Println("Sorry, I didn't understand that.")
+			continue
 		}
-		fmt.Printf("Player \033[1m%s\033[0m is color %v and will start at coordinate %v\n", name, color, startPos)
+
+		input = strings.TrimSpace(input)
+		if strings.ToLower(input) == "pass" {
+			if err := g.PassTurn(player); err != nil {
+				fmt.Printf("Sorry, I couldn't pass the player's turn. %v\n", err)
+				continue
+			}
+			fmt.Printf("Passing %s's turn.\n", highlightString(player.Name()))
+			break
+		}
+
+		i, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Sorry, I couldn't understand the piece number.")
+			continue
+		}
+		if err := player.CheckPiecePlaceability(i); err != nil {
+			fmt.Printf("Sorry, I can't place that piece. %v\n", err)
+			continue
+		}
+
+		var o blokus.Orientation
+		fmt.Print("How to orient the piece? Enter number of times to rotate 90° clockwise, followed by 'true' or 'false' for flipping horizontally. E.g. '2 false': ")
+		if err := fscanln(stdin, &o.Rot, &o.Flip); err != nil {
+			fmt.Println("Sorry, I couldn't understand the input.")
+			continue
+		}
+		o.Rot = blokus.Normalize(o.Rot)
+		fmt.Printf("Rotating the piece %d times and ", o.Rot)
+		if !o.Flip {
+			fmt.Print("not ")
+		}
+		fmt.Print("flipping horizontally.\n")
+
+		var c blokus.Coord
+		fmt.Print("Where would you like to place the piece? Enter coordinates as 'row column': ")
+		if err := fscanln(stdin, &c.X, &c.Y); err != nil {
+			fmt.Println("Sorry, I couldn't understand the coordinates.")
+			continue
+		}
+
+		if err := g.PlacePiece(player, i, o, c); err != nil {
+			fmt.Printf("Sorry, I couldn't place that piece. %v\n", err)
+			continue
+		}
+		fmt.Printf("Player %s has placed piece %d.\n", highlightString(player.Name()), i)
+		break
 	}
 	return nil
 }
@@ -102,26 +181,30 @@ func main() {
 	var p *blokus.Piece
 	p, err := blokus.NewPiece([]blokus.Coord{blokus.Coord{0, 0}})
 	if err != nil {
-		log.Fatalf("Could not create piece: %v", err)
+		log.Fatalf("Could not create piece: %v\n", err)
 	}
 	pieces = append(pieces, p)
 
 	g, err := blokus.NewGame(1, blokus.DefaultBoardSize, pieces)
 	if err != nil {
-		log.Fatalf("Could not create new game: %v", err)
+		log.Fatalf("Could not create new game: %v\n", err)
 	}
 
 	if err := promptForNewPlayers(g); err != nil {
 		log.Fatal(err.Error())
 	}
 
-	renderBoard(g.Board())
-
-	if err := g.PlacePiece(g.Players()[0], 0, blokus.Orientation{}, blokus.Coord{0, 0}); err != nil {
-		log.Fatalf("Could not place piece: %v", err)
+	for !g.IsGameEnd() {
+		renderBoard(g.Board())
+		if err := promptForNextMove(g); err != nil {
+			log.Fatalf("Could not process next move: %v\n", err)
+		}
+		if err := g.AdvanceTurn(); err != nil {
+			log.Fatalf("Could not advance turn: %v\n", err)
+		}
 	}
 
-	renderBoard(g.Board())
+	// TODO: Display score
 
-	fmt.Println("Done!")
+	fmt.Println("Someone won! Yay!")
 }

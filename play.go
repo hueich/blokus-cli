837633ee3b3641c1daab1bf4c44ce445e65760a3
Addison Luh <hueich@gmail.com>
2017-02-04 20:40:19 -0800

    Board rendering working

--- play.go
+++ play.go
@@ -1,10 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"strings"
 
 	"github.com/hueich/blokus"
 )
+
+func getColorSymbol(c blokus.Color) string {
+	switch c {
+	case blokus.Blue:
+		return "\033[1;34mB\033[0m"
+	case blokus.Yellow:
+		return "\033[1;33mY\033[0m"
+	case blokus.Red:
+		return "\033[1;31mR\033[0m"
+	case blokus.Green:
+		return "\033[1;32mG\033[0m"
+	default:
+		return " "
+	}
+}
+
+func renderBoard(b *blokus.Board) {
+	div := fmt.Sprintf("+%s", strings.Repeat("---+", len(b.Grid())))
+	fmt.Println(div)
+	for _, r := range b.Grid() {
+		fmt.Print("|")
+		for _, c := range r {
+			fmt.Printf(" %v |", getColorSymbol(c))
+		}
+		fmt.Println("")
+		fmt.Println(div)
+	}
+}
 
 func main() {
 	pieces := make([]*blokus.Piece, 0)
@@ -15,9 +45,25 @@ func main() {
 	}
 	pieces = append(pieces, p)
 
-	_, err = blokus.NewGame(1, blokus.DefaultBoardSize, pieces)
+	g, err := blokus.NewGame(1, blokus.DefaultBoardSize, pieces)
 	if err != nil {
 		log.Fatalf("Could not create new game: %v", err)
 	}
-	log.Println("Done!")
+
+	if err := g.AddPlayer("Bob", blokus.Blue, blokus.Coord{0, 0}); err != nil {
+		log.Fatalf("Could not add player: %v", err)
+	}
+	if err := g.AddPlayer("Yeti", blokus.Yellow, blokus.Coord{19, 19}); err != nil {
+		log.Fatalf("Could not add player: %v", err)
+	}
+
+	renderBoard(g.Board())
+
+	if err := g.PlacePiece(g.Players()[0], 0, blokus.Orientation{}, blokus.Coord{0, 0}); err != nil {
+		log.Fatalf("Could not place piece: %v", err)
+	}
+
+	renderBoard(g.Board())
+
+	fmt.Println("Done!")
 }
